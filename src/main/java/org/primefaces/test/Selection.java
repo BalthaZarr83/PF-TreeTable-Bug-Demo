622e1ec1182e149c0a56907b440c93066d2a2e7d
Michael <michael.herler@gmx.at>
2021-07-14 09:38:32 +0200

    TreeNode bug sample added

---
+++ src/main/java/org/primefaces/test/Selection.java
@@ -0,0 +1,12 @@
+package org.primefaces.test;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Selection {
+    private String selectionName;
+}
--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -8,6 +8,8 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
 import lombok.Data;
+import org.primefaces.model.DefaultTreeNode;
+import org.primefaces.model.TreeNode;
 
 @Data
 @Named
@@ -18,10 +20,18 @@ public class TestView implements Serializable {
     private Integer integer;
     private BigDecimal decimal;
     private LocalDateTime localDateTime;
+
+    private TreeNode baseTreeNode;
+    private TreeNode[] selectedTreeNodes;
     
     @PostConstruct  
     public void init() {
         string = "Welcome to PrimeFaces!!!";
+        baseTreeNode= new DefaultTreeNode(new Selection("baseNode"));
+        TreeNode unselTreeNode= new DefaultTreeNode(new Selection("unselectable"), baseTreeNode);
+        unselTreeNode.setSelectable(false);
+        TreeNode selectable= new DefaultTreeNode(new Selection("selectable"), unselTreeNode);
+
     }
 
 }
